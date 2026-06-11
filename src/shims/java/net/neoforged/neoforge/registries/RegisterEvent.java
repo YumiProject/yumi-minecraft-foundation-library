051ef66f70af6c00e99851b70fed7f27c63e8754
Layla <email@lambdaurora.dev>
2026-06-11 15:56:00 +0200

    Update Gradle wrapper, clean up directories.

--- src/shims/java/net/neoforged/neoforge/registries/RegisterEvent.java
+++
@@ -1,8 +0,0 @@
-package net.neoforged.neoforge.registries;
-
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceKey;
-
-public class RegisterEvent {
-	RegisterEvent(ResourceKey<? extends Registry<?>> registryKey, Registry<?> registry) {}
-}
