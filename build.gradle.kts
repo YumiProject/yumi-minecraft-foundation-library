c2d0eeb8f0fafa84ab2170ea4b5244034ab1e692
Layla <email@lambdaurora.dev>
2026-09-10 23:44:47 +0200

    Update Minecraft dependency requirements to support 26.1+.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ lambdamcdev {
 			}
 			this.withLicense(Constants.LICENSE_NAME)
 			this.withEntrypoints("yumi:init", "dev.yumi.mc.core.impl.YumiFoundationMod")
-			this.withDepend("minecraft", "~26.1-")
+			this.withDepend("minecraft", ">=26.1-")
 			this.withDepend("java", ">=${Constants.JAVA_VERSION}")
 			this.withDepend("yumi_commons_event", "~${libs.versions.yumi.commons.get()}")
 			this.withMixins("yumi_mc_core.mixins.json", "yumi_mc_core.neoforge.mixins.json")
@@ -39,7 +39,7 @@ lambdamcdev {
 		nmt {
 			fmj.copyTo(this)
 			this.withLoaderVersion("[2,)")
-			this.withDepend("minecraft", "[${libs.versions.minecraft.get()},26.2)")
+			this.withDepend("minecraft", "[${libs.versions.minecraft.get()},)")
 			this.withMixins("yumi_mc_core.mixins.json", "yumi_mc_core.neoforge.mixins.json")
 			this.withCustom("\"yumi:entrypoints\".\"yumi:init\"", "dev.yumi.mc.core.impl.YumiFoundationMod")
 		}
